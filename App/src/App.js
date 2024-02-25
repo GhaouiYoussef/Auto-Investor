3b4df964fa69b02c09f696bb7f12ded491f398b4
GhaouiYoussef <youssef.ghaoui@ensi-uma.tn>
2024-02-26 01:37:10 +0200

    added forlder app containing all

---
+++ App/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import HowItWorks from './HowItWorks';
+import Features from './features';
+const App = () => {
+  return (
+    <div>
+      <Header /> {/* Render the Header component */}
+      {/* Other components and content */}
+      <HowItWorks /> {/* Render the HowItWorks component */}
+      {/* Other components and content */}
+      <Hero /> {/* Render the Hero component */}
+      {/* Other components and content */}
+      <Features /> {/* Render the Features component */}
+      {/* Other components and content */}
+
+    </div>
+  );
+};
+
+export default App;
