3cd3c99221221a7efd0fe76191b1c72695bff3a7
khalil trabelsi <khalil.trabelsi@ensi-uma.tn>
2024-03-30 01:24:02 +0000

    hashage backend

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { Pool } = require('pg');
+const bcrypt = require('bcrypt')
 
 const app = express()
 app.listen(3001 ,() => 
@@ -24,8 +25,9 @@ app.post('/Create',async (req,res) =>{
 const {name,email,password} = req.body
 try {
     // Insert user credentials into the users table
+    const hashedPassword = await bcrypt.hash(password, 10);
     const query = 'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)';
-    await pool.query(query, [name, email ,password]);
+    await pool.query(query, [name, email, hashedPassword]);
 
     res.status(200).send('User registered successfully');
 } catch (error) {
@@ -37,18 +39,26 @@ try {
 app.post('/Login',async (req,res) =>{
     const {email,password} = req.body
     try {
-        const query = 'SELECT * FROM users WHERE email = $1 AND password = $2';
-    const result = await pool.query(query, [email, password]);
+        const query = 'SELECT * FROM users WHERE email = $1';
+        const result = await pool.query(query, [email]);
 
-    if (result.rows.length > 0) {
-        // User exists
-        res.status(200).send('User exists');
-        console.log('User exists');
-    } else {
-        // User does not exist
-        res.status(401).json({ error: 'Invalid email or password' });
-        console.log('User not found');
-    }
+        if (result.rows.length > 0) {
+            const user = result.rows[0];
+            const isPasswordMatch = await bcrypt.compare(password, user.password);
+            if (isPasswordMatch) {
+                // Passwords match
+                res.status(200).send('User logged in successfully');
+                console.log('User logged in successfully');
+            } else {
+                // Passwords do not match
+                res.status(401).json({ error: 'Invalid email or password' });
+                console.log('Invalid email or password');
+            }
+        } else {
+            // User does not exist
+            res.status(401).json({ error: 'Invalid email or password' });
+            console.log('User not found');
+        }
     } catch (error) {
         console.error('Error inserting user', error);
         res.status(500).send('Internal server error');
