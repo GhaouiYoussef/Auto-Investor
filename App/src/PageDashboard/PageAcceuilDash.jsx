56bebda84597ea3cf348979b9cd5dd56fee7461e
GhaouiYoussef <youssef.ghaoui@ensi-uma.tn>
2024-04-02 02:28:26 +0100

    fix dash

--- App/src/PageDashboard/PageAcceuilDash.jsx
+++ App/src/PageDashboard/PageAcceuilDash.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import './styles/global.scss'
+import React from 'react';
+import { Link, Route, Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from './Profile';
+import Packages from "./Packages";
+import Navbar from './navbar/Navbar';
+import Footer from './footer/Footer';
+import Menu from './menu/Menu';
+import Logout from './logout/Logout';
 
 const PageAcceuilDash = () => {
-  return (
-    <div>PageAcceuilDash</div>
-  )
-}
+  // const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  // };
 
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/Dashboard",
+  //     element: <Layout />,
+  //     children: [
+  //       {
+  //         path: "PageAcceuilDash",
+  //         element: <PageAcceuilDash />
+  //       },
+  //       {
+  //         path: "profile",
+  //         element: <Profile />
+  //       },
+  //       {
+  //         path: "packages",
+  //         element: <Packages />
+  //       },
+
+  //     ]
+  //   },
+  //   {
+  //     path: "/Dashboard/logout",
+  //     element: <Logout />
+  //   }
+  // ]);
+
+  // return <RouterProvider router={router} />;
+};
 export default PageAcceuilDash
