da9e9e272285ad9d1958f832e12a1bc9850431aa
GhaouiYoussef <youssef.ghaoui@ensi-uma.tn>
2024-04-12 18:35:32 +0100

    --

--- App/src/PageDashboard/Packages.jsx
+++ App/src/PageDashboard/Packages.jsx
@@ -1,12 +1,51 @@
-import React from "react";
-
+import React from 'react';
+import './Packages.css';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import { Link } from 'react-router-dom';
 
 const Packages = () => {
   return (
-    <div>
-      <h1> Packages</h1>
+    <div className="packages-container">
+      <div className="packages-heading">
+        <h1>Our Trading Packages</h1>
+       
+        <p>Grow your money with our stable and profitable packages that suit you best!</p>
+      </div>
+      <div className="packages">
+        <div className="package">
+          <h2>Basic Package</h2>
+          <p>$10 per month</p>
+          <p>Access to basic features</p>
+          <p>7-day free trial</p>
+          <Link to="/dashboard/InvestmentPlanRegistration" style={{ textDecoration: 'none' }} className='link-box'>
+          <AppRegistrationIcon className="icon" />
+          <p>Invest now</p>
+          </Link>
+          
+        </div>
+        <div className="package">
+          <h2>Auto-Advisor Package</h2>
+          <p>$20 per month</p>
+          <p>Access to advanced features</p>
+          <p>7-day free trial</p>
+          <Link to="/dashboard/InvestmentPlanRegistration" style={{ textDecoration: 'none' }} className='link-box'>
+          <AppRegistrationIcon className="icon" />
+          <p>Invest now</p>
+          </Link>
+        </div>
+        <div className="package">
+          <h2>Auto-Investor Package</h2>
+          <p>$30 per month</p>
+          <p>Access to premium features</p>
+          <p>7-day free trial</p>
+          <Link to="/dashboard/InvestmentPlanRegistration" style={{ textDecoration: 'none' }} className='link-box'>
+          <AppRegistrationIcon className="icon" />
+          <p>Invest now</p>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Packages
+export default Packages;
--- App/src/PageDashboard/PageAcceuilDash.jsx
+++ App/src/PageDashboard/PageAcceuilDash.jsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import axios from "axios";
-
 import Sidebar from "./Sidebar/Sidebar";
+import './PageAccueilDash.scss';
 import Widget from "./widget/Widget";
 import Featured from './featured/Featured';
 import Chart from './chart/Chart';
-const PageAcceuilDash = () => {
-  axios.defaults.withCredentials = true;// to force credentials to every Axios requests
-const handleLogout = () => {
-  
-  axios
-    .get("http://localhost:3001/logout")
-    .then((res) => {
-      if (res.data.status === "200") {
-        window.location.href = '/';
-      } else {
-        alert("error");
-      }
-    })
-    .catch((err) => console.log(err));
-};
 
+const PageAcceuilDash = () => {
   return (
     <div className="PageAcceuilDash">
       <Sidebar />
